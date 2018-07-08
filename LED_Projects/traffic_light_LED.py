d51410649155604e36154f6cbbc87683414517bd
Michael Wiggin <myklobe@gmail.com>
2018-07-07 22:14:36 -0500

    Update traffic_light_LED.py

--- LED_Projects/traffic_light_LED.py
+++ LED_Projects/traffic_light_LED.py
@@ -11,15 +11,15 @@ GPIO.setwarnings(False)
 
 i = 0
 while i < 10: #Loops 10 times, but you can make cycle indefinitely or for a specific # of times
-	setupLED.redLEDOn()
+	setupLED.redLEDOn() # Red Light on for 5 seconds
 	time.sleep(5000)
-	setupLED.redLEDOff()
+	setupLED.redLEDOff() # Red Light off
 
-	setupLED.yellowLEDOn()
+	setupLED.yellowLEDOn() # Yellow light on for 2 seconds
 	time.sleep(2000)
-	setupLED.yellowLEDOff()
+	setupLED.yellowLEDOff() # Yellow light off
 	
-	setupLED.greenLEDOn()
+	setupLED.greenLEDOn() # Green light on for 5 seconds
 	time.sleep(5000)
-	setupLED.greenLEDOff()
+	setupLED.greenLEDOff() # Green light off
 	i += 1
